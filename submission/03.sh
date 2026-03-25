aed995e8331b7a79754ee919655c583d8991bbb7
sawe-daisy <deesawe16@gmail.com>
2026-03-25 14:01:44 +0300

    add assignments

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,4 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+addr=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" bech32) && bitcoin-cli -regtest generatetoaddress 101 "$addr" >/dev/null && echo "$addr"
